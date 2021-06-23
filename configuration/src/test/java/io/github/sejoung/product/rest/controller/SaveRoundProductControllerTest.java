364578609b260e2c44693fc5f23d681fe1a0fe4b
sejoung kim <sejoung@gmail.com>
2021-06-23 18:16:13 +0900

    테스트 추가

--- configuration/src/test/java/io/github/sejoung/product/rest/controller/SaveRoundProductControllerTest.java
+++ configuration/src/test/java/io/github/sejoung/product/rest/controller/SaveRoundProductControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
